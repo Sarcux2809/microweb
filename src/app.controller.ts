3fe0a4f9e8fb96cfe71299d7b3dcf4e3a2f100a5
Sarcux <hiram.2809@gmail.com>
2024-05-08 00:31:05 -0600

    Changes to be committed: 	modified:   public/confirmation.html 	modified:   public/existencia.html 	modified:   src/app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,48 +1,49 @@
 import { Body, Controller, Post, Res, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { datoService } from './app.service';
-import * as path from 'path'; // Importa el módulo 'path' para trabajar con rutas de archivos
+import * as path from 'path';
+import * as Redis from 'redis';
 
 @Controller()
 export class AppController {
-  getHello(): any {
-    throw new Error('Method not implemented.');
-  }
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Post()
   newUser(@Body() body: any): string {
+    // Aquí puedes procesar los datos recibidos como desees
+    // Por ejemplo, puedes guardarlos en Redis
+    const redisClient = Redis.createClient();
+    redisClient.set('nombre_de_la_clave', JSON.stringify(body));
+    redisClient.quit(); // Cierra la conexión con Redis
     return this.appService.newUser(body);
   }
 
   @Get('confirmation')
   getConfirmation(@Res() res): void {
-    // Obtén la ruta absoluta del archivo HTML
     const filePath = path.join(__dirname, '..', 'public', 'confirmation.html');
-    // Envía el archivo HTML como respuesta
     res.sendFile(filePath);
   }
 }
 
-
 @Controller('register')
 export class RegistrosController {
-  constructor(private readonly micappControllerBd: datoService) { }
+  constructor(private readonly micappControllerBd: datoService) {}
 
   @Post()
-  async createRegistro(@Body('Usuario') Usuario: string, @Body('Contraseña') Contraseña: string, @Body('Correo') Correo: string, @Res() res) {
-    // Verificar si el correo electrónico ya está registrado en la base de datos
+  async createRegistro(
+    @Body('Usuario') Usuario: string,
+    @Body('Contraseña') Contraseña: string,
+    @Body('Correo') Correo: string,
+    @Res() res,
+  ) {
     const existeUsuario = await this.micappControllerBd.findUserByEmail(Correo);
     if (existeUsuario) {
-      // Redirigir al usuario a la página de confirmación de registro existente
       res.redirect('existencia.html');
       return;
     }
 
-    // Procesar los datos y guardarlos en la base de datos
     await this.micappControllerBd.createRegistro(Usuario, Contraseña, Correo);
 
-    // Redirigir al usuario a la página de confirmación de registro exitoso
     res.redirect('/confirmation');
   }
 }
